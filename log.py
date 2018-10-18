c103ccf3da89d22b00bd45c2c682b541b15fb18b
Jesus Diaz <jesusdiaz24@tamu.edu>
2018-10-17 19:47:38 -0500

    indentation

--- log.py
+++ log.py
@@ -29,8 +29,8 @@ TOTAL_WEEK_REQUESTS = 0
 
 if not os.path.isfile(LOCAL_FILE):
 	print("Downloading log file from: {0} to {1}/{2}".format(URL_PATH,os.path.dirname(sys.argv[0]),LOCAL_FILE))
-local_file, headers = urlretrieve(URL_PATH,LOCAL_FILE)
-print("\nFile{} saved to directory.".format(LOCAL_FILE))
+    local_file, headers = urlretrieve(URL_PATH,LOCAL_FILE)
+    print("\nFile{} saved to directory.".format(LOCAL_FILE))
 
 fh = open(LOCAL_FILE)
 
